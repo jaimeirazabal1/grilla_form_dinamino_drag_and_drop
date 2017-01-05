9cd10d9ad5dc9afcf6428914902139bcf029e3e9
Jaime Irazabal <jaimeirazabal1@gmail.com>
2017-01-05 11:27:54 -0430

    ya hace los tabs dinamicos y pueden pasarse los inputs entre tabs

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
+var tabs_editables = 0;
 $(document).ready(function(){
-
-	var inputs = $("#main form").find('input');
-
-	inputs.each(function(i){
-		console.log($(this).prop('tagName'))
-	});
-
-	var tool_bar = '<div id="tool_bar"></div>';
-	$("#main form").prepend(tool_bar)
+	var tool_bar = '<div id="tool_bar_'+tabs_editables+'" class="tool_bar"></div>';
 	var controles = '<ul class="nav nav-tabs">'+
   		'<li role="presentation" class="dropdown">'+
     		'<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Agregar Filas <span class="caret"></span></a>'+
@@ -29,12 +22,52 @@ $(document).ready(function(){
     '</ul>'+
   '</li>'+
 '</ul>';
-	$("#tool_bar").prepend(controles)
+
+	/*codigo para los tabs*/
+	$(".tabs-container").find("a").append("&nbsp;&nbsp;<button class='btn btn-success btn-xs btn_agregar_tabs'>+</button>");
+	$("body").on("click",".btn_agregar_tabs",function(){
+		tabs_editables++;
+		$(".tabs-container ul").eq(0).find("li.active").removeClass('active');
+		$(".tabs-container ul").eq(0).append("<li class='active'><a data-toggle='tab' id='"+tabs_editables+"' class='tab_editable_"+tabs_editables+"' contenteditable='true'>Escribe el Nombre aqui</a></li>");
+		$(".tab-content").append("<div id='' class='tab-pane content_tab_"+tabs_editables+"'><div class='panel-body'><h1>Contenido del tab "+tabs_editables+"</h1></div></div>")
+		tool_bar_dinamic = '<div id="tool_bar_'+tabs_editables+'" class="tool_bar"></div>';
+		$(".content_tab_"+tabs_editables+" .panel-body").append(tool_bar_dinamic);
+		$('#tool_bar_'+tabs_editables).append(controles)
+		$("#"+tabs_editables).droppable({
+		    activeClass: "ui-state-highlight",
+		    drop: function (event, ui) {
+		        $(this).attr("id")
+		        $(ui.draggable).remove()
+		        $(".content_tab_"+tabs_editables+" .panel-body").append('<div class="control-group" data-drop="true">'+ui.draggable.html()+'</div>')
+		        //$(".tabs-container").tabs("refresh");
+		        $('.control-group').draggable();
+		    }
+		});
+	});
+
+	$("body").on('keyup', '[contenteditable]', function() {
+	    $(this).attr("href","#tab"+$(this).text().replace(/ /g,"_"));
+	    $(".content_tab_"+$(this).attr("id")).attr("id","tab"+$(this).text().replace(/ /g,"_"))
+	});
+	$("body").on('blur', '[contenteditable]', function() {
+	    $(this).attr("contenteditable",false)
+	});
+	var inputs = $("#main form").find('input');
+
+	inputs.each(function(i){
+		console.log($(this).prop('tagName'))
+	});
+
+
+	$("#tabDatos_Generales").prepend(tool_bar);
+
+	$("#tool_bar_"+tabs_editables).prepend(controles)
 
 	$('body').on('click',".una_fila_una_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-12 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -51,9 +84,11 @@ $(document).ready(function(){
 			
 	});
 	$('body').on('click',".una_fila_dos_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-6 gris"></div><div class="col-md-6 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -71,9 +106,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_tres_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-4 gris"></div><div class="col-md-4 gris"></div><div class="col-md-4 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -91,9 +128,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_cuatro_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-3 gris"></div><div class="col-md-3 gris"></div><div class="col-md-3 gris"></div><div class="col-md-3 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -111,9 +150,11 @@ $(document).ready(function(){
 	});
 
 	$('body').on('click',".una_fila_cinco_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -131,9 +172,11 @@ $(document).ready(function(){
 	});
 
 	$('body').on('click',".una_fila_seis_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div><div class="col-md-2 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -150,9 +193,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_siete_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -169,9 +214,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_ocho_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -188,9 +235,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_nueve_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -207,9 +256,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_diez_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -226,9 +277,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_once_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -246,9 +299,11 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_doce_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div><div class="col-md-1 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
@@ -265,16 +320,18 @@ $(document).ready(function(){
 		});		
 	});
 	$('body').on('click',".una_fila_ochocuatro_columna",function(){
+		id_tab = $(this).parent().parent().parent().parent().parent().attr("id").split("_")[2];
+
 		equis = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>'
 		div = '<div class="row"><div class="col-md-8 gris"></div><div class="col-md-4 gris"></div>'+equis+'</div>';
-		$("#tool_bar").append(div);
+		$("#tool_bar_"+id_tab).append(div);
 		$( ".gris" ).droppable({
 		   // accept: ".control-group",
 		   hoverClass: "hover",
 		    drop: function( event, ui ) {
 		    	console.log('soltaron algo por aqui')
 		    	$(this).parent().find('.close').remove();
-		    	
+
 		       	var droppable = $(this);
 		       	var draggable = ui.draggable;
 		       	draggable.prop('style','');
