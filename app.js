f73a2d60f066ec33135c58372bad23e2bcfda682
Jaime Irazabal <jaimeirazabal1@gmail.com>
2017-01-08 14:51:40 -0400

    ya agrega select y los demas solo falta el date

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ $(document).ready(function(){
 		    '<ul class="dropdown-menu">';
 
 		    	for (var i = 0; i < herramientas_array.length ; i++) {
-		    		console.log(herramientas_array[i].categoria,buscarCategoria(herramientas_array[i].categoria,plugins_categories))
+		    		// console.log(herramientas_array[i].categoria,buscarCategoria(herramientas_array[i].categoria,plugins_categories))
 		    		if (buscarCategoria(herramientas_array[i].categoria,plugins_categories)) {
 		    			//$('.'+herramientas_array[i].categoria).append('<li><a href="#">'+herramientas_array[i].categoria+'</a></li>')
 		    		}else{
@@ -137,7 +137,7 @@ $(document).ready(function(){
 		tabs_editables++;
 		boton_cerrar = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>';
 		$(".tabs-container ul").eq(0).find("li.active").removeClass('active');
-		$(".tabs-container ul").eq(0).append("<li class='active'><a data-toggle='tab' id='"+tabs_editables+"' class='tab_editable_"+tabs_editables+"' contenteditable='true'>Escribe el Nombre aqui</a>"+boton_cerrar+"</li>");
+		$(".tabs-container ul").eq(0).append("<li class='active'><a data-toggle='tab' id='"+tabs_editables+"' class='tab_editable_"+tabs_editables+" tabsita' contenteditable='true'>Escribe el Nombre aqui</a>"+boton_cerrar+"</li>");
 		$(".tab-content").append("<div id='' class='tab-pane content_tab_"+tabs_editables+"'><div class='panel-body'><h1 contenteditable='true'>Contenido del tab "+tabs_editables+"</h1></div></div>")
 		tool_bar_dinamic = '<div id="tool_bar_'+tabs_editables+'" class="tool_bar"></div>';
 		$(".content_tab_"+tabs_editables+" .panel-body").append(tool_bar_dinamic);
@@ -150,7 +150,7 @@ $(document).ready(function(){
 		$("#"+tabs_editables).droppable({
 		    activeClass: "ui-state-highlight",
 		    drop: function (event, ui) {
-		        console.log();
+		        // console.log();
 		        $(ui.draggable).remove()
 		        $(".content_tab_"+$(this).attr('class').split(" ")[0].split("_")[2]+" .panel-body").append('<div class="control-group" data-drop="true">'+ui.draggable.html()+'</div>')
 		        //$(".tabs-container").tabs("refresh");
@@ -159,17 +159,23 @@ $(document).ready(function(){
 		});
 	});
 
-	$("body").on('keyup', '[contenteditable]', function() {
-	    $(this).attr("href","#tab"+$(this).text().replace(/ /g,"_"));
-	    $(".content_tab_"+$(this).attr("id")).attr("id","tab"+$(this).text().replace(/ /g,"_"))
+	$("body").on('keyup', '.tabsita', function() {
+		console.log($(this).attr("id"))
+		// if (!$(this).attr("id")) {
+
+		    $(this).attr("href","#tab"+$(this).text().replace(/ /g,"_"));
+		    $(".content_tab_"+$(this).attr("id")).attr("id","tab"+$(this).text().replace(/ /g,"_"))
+		// }
 	});
-	$("body").on('blur', '[contenteditable]', function() {
-	    $(this).attr("contenteditable",false)
+	$("body").on('blur', '.tabsita', function() {
+		// if (!$(this).attr("id")) {
+	    	$(this).attr("contenteditable",false)
+		// }
 	});
 	var inputs = $("#main form").find('input');
 
 	inputs.each(function(i){
-		console.log($(this).prop('tagName'))
+		// console.log($(this).prop('tagName'))
 	});
 
 
@@ -615,12 +621,13 @@ $(document).ready(function(){
 	$("body").on("click",".close",function(){
 		$(this).parent().remove();
 	});
-	var modal_cambio_input = '<div class="modal fade" id="myModalCambioInput" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"> <div class="modal-dialog modal-lg" role="document"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> <h4 class="modal-title" id="myModalLabel">Modal title</h4> </div> <div class="modal-body" id="body_cambio_input"></div> <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button> <button type="button" class="btn btn-primary" id="guardar_cambio_input">Guardar</button> </div> </div> </div> </div>';
+	var modal_cambio_input = '<div class="modal fade" id="myModalCambioInput" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"> <div class="modal-dialog modal-lg" role="document"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> <h4 class="modal-title" contenteditable="true" id="myModalLabel">Modal title</h4> </div> <div class="modal-body" id="body_cambio_input"></div> <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button> <button type="button" class="btn btn-primary" id="guardar_cambio_input">Guardar</button> </div> </div> </div> </div>';
 	$("#main").append(modal_cambio_input)
 	$('.control-group').draggable();
 
 	$('body').on("contextmenu",".control-group",function(e){
 		e.preventDefault();
+		select_values = undefined;
 		controlGroup = $(this);
 		$('#myModalCambioInput').modal('show');
 		var label = $(this).find(".control-label");
@@ -629,17 +636,32 @@ $(document).ready(function(){
 
 		type = $(this).find(".controls").eq(0).children().eq(0).attr("type");
 		tagName = $(this).find(".controls").eq(0).children().eq(0).prop("tagName");
-		console.log(type,tagName)
-		if (type=="text" && tagName == "INPUT") {
+		// console.log(type,tagName)
+		if (type=="text" && tagName == "INPUT" || type=="textarea" && tagName == "TEXTAREA") {
 			input = $(this).find(".controls").eq(0).children().eq(0);
 				body += "<div class='col-md-6 panel panel-default'>"+
-							"<center><h3>Input Type</h3></center>"+
-							"<div class='textarea_cambio_div'>"+
+							"<center><h3>Input Type</h3></center>";
+					if (type == 'text') {
+
+						body +=	"<div class='textarea_cambio_div'>"+
 								"<label><input type='radio' name='text_cambio' checked value='text' class='text_cambio'> Text</label>"+
 							"</div>";
-					body += "<div class='textarea_cambio_div'>"+
-								"<label><input type='radio' name='text_cambio' value='textarea' class='text_cambio'> Textarea</label>"+
+					}else{
+						body +=	"<div class='textarea_cambio_div'>"+
+								"<label><input type='radio' name='text_cambio' value='text' class='text_cambio'> Text</label>"+
 							"</div>";
+					}
+					
+					if (tagName == 'TEXTAREA') {
+						body += "<div class='textarea_cambio_div'>"+
+									"<label><input type='radio' name='text_cambio' checked value='textarea' class='text_cambio'> Textarea</label>"+
+								"</div>";
+					}else{
+
+						body += "<div class='textarea_cambio_div'>"+
+									"<label><input type='radio' name='text_cambio' value='textarea' class='text_cambio'> Textarea</label>"+
+								"</div>";
+					}
 					body += "<div class='textarea_cambio_div'>"+
 								"<label><input type='radio' name='text_cambio' value='select' class='text_cambio'> Select</label>"+
 							"</div>";
@@ -655,7 +677,67 @@ $(document).ready(function(){
 						"<br></div>";
 
 		}
+		if ((typeof type == "undefined" && tagName == "SELECT") || (type == "select" && tagName == "SELECT")) {
+			
+			input = $(this).find(".controls").eq(0).children().eq(0);
+			// console.log($(this).find(".controls").eq(0))
+			// console.log(input.data());
+				body += "<div class='col-md-6 panel panel-default'>"+
+							"<center><h3>Input Type</h3></center>";
+					
+					body +=	"<div class='textarea_cambio_div'>"+
+								"<label><input type='radio' name='text_cambio' value='text' class='text_cambio'> Text</label>"+
+							"</div>";
+					
+					body += "<div class='textarea_cambio_div'>"+
+								"<label><input type='radio' name='text_cambio' value='textarea' class='text_cambio'> Textarea</label>"+
+							"</div>";
+					
+					body += "<div class='textarea_cambio_div'>"+
+								"<label><input type='radio' name='text_cambio' checked value='select' class='text_cambio'> Select</label>"+
+							"</div>";
+					body += "<div class='textarea_cambio_div'>"+
+								"<label><input type='radio' name='text_cambio' value='radio' class='text_cambio'> Date</label>"+
+							"</div>"+
+						"</div>";
+
+				body += "<div class='col-md-6 panel panel-default'>"+
+							"<center><span style='padding:10px;font-size:16px;font-weight: 600;margin-top:10px'>Propiedades</span>&nbsp;&nbsp;<button class='btn btn-primary btn-xs' id='add_propiedades' style='margin-top:4px;'>+</button></center>"+
+							"<div class='panel_propiedades'>"+
+
+							"</div>"+
+						"<br></div>";
+
+			select_values = "<div class='row'>";
+				select_values += "<div class='col-md-6 panel panel-default panel_options' style='padding-bottom:8px;'>";
+				select_values += "<center><span style='padding:10px;font-size:16px;font-weight: 600;margin-top:10px'>Select Options</span>&nbsp;&nbsp;<button class='btn btn-primary btn-xs' id='add_options' style='margin-top:4px;'>+</button></center>";
+						input.find('option').each(function(i){
+							if ($(this).text()) {
+								// console.log('input:',i)
+								select_values += "<div class='row'>";
+									select_values += "<div class='col-md-4'>";
+										select_values += "<input type='text' value='"+$(this).val()+"' class='form-control option_valor'>"
+									select_values += "</div>";
+									select_values += "<div class='col-md-5'>";
+										select_values += "<input type='text' value='"+$(this).text()+"' class='form-control option_texto'>"
+									select_values += "</div>";
+									select_values += "<div class='col-md-1'>";
+										select_values += "<button class='cerrar btn btn-primary btn-xs'>x</button>"
+									select_values += "</div>";
+								select_values += "</div>";
+							}
+						})
+				select_values += "</div>";
+			select_values += "</div>";
+
+			// console.log(select_values)
+			
+
+		}
+
+
 		$("#body_cambio_input").html("<div class='row'>"+body+"</div>");
+
 			nueva = "<div class='row'>"+
 				"<div class='col-xs-6'><input type='text' placeholder='Class' value='class' class='form-control propiedad_nombre'></div>"+
 				"<div class='col-xs-6'><input type='text' placeholder='Valor Class' value='"+input.attr("class")+"' class='form-control propiedad_valor'></div>"+
@@ -671,6 +753,28 @@ $(document).ready(function(){
 				"<div class='col-xs-6'><input type='text' placeholder='Valor Class' value='"+input.attr("name")+"' class='form-control propiedad_valor'></div>"+
 			"</div>";
 			$(".panel_propiedades").append(nueva);
+
+			attributes_select = input.data();
+
+			for (attr in attributes_select) {
+				if (attr) {
+
+					nueva = "<div class='row'>"+
+						"<div class='col-xs-6'><input type='text' placeholder='Class' value='data-"+attr+"' class='form-control propiedad_nombre'></div>"+
+						"<div class='col-xs-6'><input type='text' placeholder='Valor Class' value='"+attributes_select[attr]+"' class='form-control propiedad_valor'></div>"+
+					"</div>";
+					$(".panel_propiedades").append(nueva);				
+				}
+			}
+
+		$(".panel_options").parent().remove();
+		
+			if (typeof select_values != "undefined") {
+				// alert("?")
+				$("#body_cambio_input").append(select_values);
+			}
+			
+
 	})
 	$("body").on("click","#add_propiedades",function(){
 		nueva = "<div class='row'>"+
@@ -682,21 +786,124 @@ $(document).ready(function(){
 	});
 
 	$("body").on("click","#guardar_cambio_input",function(){
+
+		var propiedades='';
+		var tipo='';
+
 		$(".text_cambio").each(function(){
 			if ($(this).is(":checked")) {
 				tipo = $(this).val();
-				console.log('tipo:',tipo);
+				propiedades+= ' type="'+tipo+'" ';
+				// console.log('tipo:',tipo);
 			}
 		});
 
 		$(".propiedad_nombre").each(function(i){
 			console.log($(this).val(),$('.propiedad_valor').eq(i).val());
+			//si el valor de la propiedad no viene vacio, lo asigna
+			if ($('.propiedad_valor').eq(i).val()) {
+
+				if ($(this).val == 'value') {
+					value=$('.propiedad_valor').eq(i).val();
+				}
+				propiedades+= $(this).val()+"='"+$('.propiedad_valor').eq(i).val()+"' ";
+			}
+
 		})
+		if (typeof value == 'undefined') {
+			value = '';
+		}
+		if (tipo == 'textarea') {
+			nuevo_input = "<textarea "+propiedades+" >"+value+"</textarea>";
+		}
+		if (tipo == 'text') {
+			nuevo_input = "<input "+propiedades+" >";
+		}
+		if (tipo == 'select') {
+			nuevo_input = "<select "+propiedades+" >";	
+			$(".option_valor").each(function(i){
+				nuevo_input += "<option value='"+$(this).val()+"'>"+$('.option_texto').eq(i).val()+"</option>";	
+			})
+			nuevo_input += "</select>";	
+		}
+		// console.log(nuevo_input)
+		// console.log(controlGroup)
+		labelName = $("#myModalLabel").text();
+		controlGroup.html("<label class='control-label'>"+labelName+"</label><div class='controls'>"+nuevo_input+"</div>");
+		$('#myModalCambioInput').modal('hide');
+
 	});
 
 	$("body").on("click",".text_cambio",function(){
-		$(this).attr("checked",true)
+		$(this).attr("checked",true);
+		if ($(this).val() == 'select') {
+			select_values = "<div class='row'>";
+				select_values += "<div class='col-md-6 panel panel-default panel_options' style='padding-bottom:8px;'>";
+				select_values += "<center><span style='padding:10px;font-size:16px;font-weight: 600;margin-top:10px'>Select Options</span>&nbsp;&nbsp;<button class='btn btn-primary btn-xs' id='add_options' style='margin-top:4px;'>+</button></center>";
+				
+								// console.log('input:',i)
+								select_values += "<div class='row'>";
+									select_values += "<div class='col-md-4'>";
+										select_values += "<input type='text' value='' class='form-control option_valor'>"
+									select_values += "</div>";
+									select_values += "<div class='col-md-5'>";
+										select_values += "<input type='text' value='' class='form-control option_texto'>"
+									select_values += "</div>";
+									select_values += "<div class='col-md-1'>";
+										select_values += "<button class='cerrar btn btn-primary btn-xs'>x</button>"
+									select_values += "</div>";
+								select_values += "</div>";
+						
+				select_values += "</div>";
+			select_values += "</div>";	
+			if ($('.panel_options').length == 0) {
+				// alert("No habia")
+				$("#body_cambio_input").append(select_values);
+			}else{
+				// alert("Si habia")
+
+				select_values = "<center><span style='padding:10px;font-size:16px;font-weight: 600;margin-top:10px'>Select Options</span>&nbsp;&nbsp;<button class='btn btn-primary btn-xs' id='add_options' style='margin-top:4px;'>+</button></center>";
+				
+								// console.log('input:',i)
+								select_values += "<div class='row'>";
+									select_values += "<div class='col-md-4'>";
+										select_values += "<input type='text' value='' class='form-control option_valor'>"
+									select_values += "</div>";
+									select_values += "<div class='col-md-5'>";
+										select_values += "<input type='text' value='' class='form-control option_texto'>"
+									select_values += "</div>";
+									select_values += "<div class='col-md-1'>";
+										select_values += "<button class='cerrar btn btn-primary btn-xs'>x</button>"
+									select_values += "</div>";
+								select_values += "</div>";
+						
+				select_values += "</div>";
+				$(".panel_options").html(select_values);
+				
+			}	
+		}else{
+			$(".panel_options").remove();
+		}
 	})
+
+	$("body").on("click",".cerrar",function(){
+		$(this).parent().parent().remove();
+	})
+
+	$("body").on("click","#add_options",function(){
+		select_values = "<div class='row'>";
+			select_values += "<div class='col-md-4'>";
+				select_values += "<input type='text' value='' class='form-control option_valor'>"
+			select_values += "</div>";
+			select_values += "<div class='col-md-5'>";
+				select_values += "<input type='text' value='' class='form-control option_texto'>"
+			select_values += "</div>";
+			select_values += "<div class='col-md-1'>";
+				select_values += "<button class='cerrar btn btn-primary btn-xs' >x</button>"
+			select_values += "</div>";
+		select_values += "</div>";
+		$(".panel_options").append(select_values);
+	});
 	// $(".dropdown-menu li").hover(function(e){
 	// 	console.log($(this).css('position'));
 	// 	$(".sub-menu").css("top",$(this).css('top')-10)
